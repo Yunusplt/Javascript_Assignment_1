fc83b2af4a853a67db75f77a8ba258995a555add
yunus <yunuspolat2528@gmail.com>
2023-04-24 15:42:39 +0200

    Assignment_7 is completed.

---
+++ Assignment_7_Todo_App/app.js
@@ -0,0 +1,36 @@
+
+
+
+const addButton = document.querySelector("#todo-button")
+const myTask = document.querySelector("#todo-input")
+const newLi = document.querySelector("#todo-ul")
+
+
+addButton.onclick=()=>{
+    newLi.innerHTML += `<li class="listem">
+    <i class="fa-solid fa-check"></i> 
+    <p class="parag">${myTask.value}</p>  
+    <i class="fa-solid fa-trash-can">
+    </i></li>`
+    myTask.value=""
+    document.querySelector("#toplam").textContent = +(document.querySelector("#toplam").textContent) + 1
+    // document.querySelector(".name").style.color="red"
+
+}
+
+// todo neden olmadi ?????#########################
+// document.querySelector(".fa-check").onclick=()=>{
+
+//     document.querySelector(".name").style.color="red"
+
+
+// }
+//todo #############################################
+
+myTask.onkeydown=(taste)=>{
+
+    if(taste.keyCode===13){
+        addButton.click()
+    }
+}
+
