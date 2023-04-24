ceef3443f80d3d25e17319be68e2b51f2859c204
yunus <yunuspolat2528@gmail.com>
2023-04-24 16:03:08 +0200

    Assignment 7 last

--- Assignment_7_Todo_App/app.js
+++ Assignment_7_Todo_App/app.js
@@ -7,24 +7,22 @@ const newLi = document.querySelector("#todo-ul")
 
 
 addButton.onclick=()=>{
-    newLi.innerHTML += `<li class="listem">
+    newLi.innerHTML = `<li class="listem">
     <i class="fa-solid fa-check"></i> 
     <p class="parag">${myTask.value}</p>  
     <i class="fa-solid fa-trash-can">
-    </i></li>`
+    </i></li>` + newLi.innerHTML
     myTask.value=""
     document.querySelector("#toplam").textContent = +(document.querySelector("#toplam").textContent) + 1
     // document.querySelector(".name").style.color="red"
 
+
 }
 
 // todo neden olmadi ?????#########################
-// document.querySelector(".fa-check").onclick=()=>{
-
-//     document.querySelector(".name").style.color="red"
-
-
-// }
+document.querySelector(".fa-check").onclick=()=>{
+console.log("hello");
+document.querySelector(".parag").style.textDecoration = "line-through"}
 //todo #############################################
 
 myTask.onkeydown=(taste)=>{
