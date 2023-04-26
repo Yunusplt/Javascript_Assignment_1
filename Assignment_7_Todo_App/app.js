787a304a7f36af663c1a35054efc07e88586b4ba
yunus <yunuspolat2528@gmail.com>
2023-04-26 09:03:25 +0200

    Assignment 7

--- Assignment_7_Todo_App/app.js
+++ Assignment_7_Todo_App/app.js
@@ -1,29 +1,48 @@
 
-
-
 const addButton = document.querySelector("#todo-button")
 const myTask = document.querySelector("#todo-input")
 const newLi = document.querySelector("#todo-ul")
 
+const myList = []
 
 addButton.onclick=()=>{
-    newLi.innerHTML = `<li class="listem">
-    <i class="fa-solid fa-check"></i> 
+
+    if (myList.includes(myTask.value)) {
+        alert("Girdiginiz deger zaten listede bulunmaktadir.")
+    }else{
+
+    newLi.innerHTML = `<li>
+    <i id="firstEmo" class="fa-solid fa-check"></i> 
     <p class="parag">${myTask.value}</p>  
-    <i class="fa-solid fa-trash-can">
+    <i id="secondEmo" class="fa-solid fa-trash-can">
     </i></li>` + newLi.innerHTML
+    myList.push(myTask.value)
     myTask.value=""
     document.querySelector("#toplam").textContent = +(document.querySelector("#toplam").textContent) + 1
-    // document.querySelector(".name").style.color="red"
-
-
+}
 }
 
-// todo neden olmadi ?????#########################
-document.querySelector(".fa-check").onclick=()=>{
-console.log("hello");
-document.querySelector(".parag").style.textDecoration = "line-through"}
-//todo #############################################
+console.log(myList);
+
+document.addEventListener("click", (e)=>{
+    if (e.target.id == "firstEmo") {
+
+        if (e.target.parentElement.className == "checked") {
+            e.target.parentElement.className = "nochecked"
+           document.querySelector("#tamamlanan").textContent = (document.querySelector("#tamamlanan").textContent) - 1
+        }else{
+        e.target.parentElement.className = "checked"
+        document.querySelector("#tamamlanan").textContent = +(document.querySelector("#tamamlanan").textContent) + 1
+        }
+    }    
+})
+
+document.addEventListener("click", (e) =>{
+    if (e.target.id=="secondEmo") {
+        e.target.parentElement.remove()
+        document.querySelector("#toplam").textContent = (document.querySelector("#toplam").textContent) - 1
+    }
+})
 
 myTask.onkeydown=(taste)=>{
 
@@ -31,4 +50,5 @@ myTask.onkeydown=(taste)=>{
         addButton.click()
     }
 }
+
 
