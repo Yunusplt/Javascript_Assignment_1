8b1abf21c764424a2ecc8899b1f12201a74a126d
yunus <yunuspolat2528@gmail.com>
2023-05-04 18:40:20 +0200

    imgages were found

--- Assignment_8_Weather_App/weather.js
+++ Assignment_8_Weather_App/weather.js
@@ -1,17 +1,26 @@
-const city = ""
 
-document.querySelector("button").onclick=()=>{
-    const city = document.querySelector("input").value
-    console.log(city);
+const cities = []
+const city = document.querySelector(".input1");
 
-}
+document.querySelector(".submit").onclick = () => {
+  cities.push(city.value)
+  console.log(city.value);
+  veriGetir();
+  city.value = ""; // input kutusu bos gözükmesi icin.
+};
 
+city.onkeydown = (taste) => {
+  if (taste.keyCode === 13) {
+    document.querySelector(".submit").click();
+  }
+};
 
+console.log(city.value);
 
 const veriGetir = async ()=>{
     const response = await fetch(
       //   `https://api.openweathermap.org/data/2.5/weather?q=london&appid=f56f703795a90532f7b92917734c7e64&units=metric`
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f56f703795a90532f7b92917734c7e64&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=f56f703795a90532f7b92917734c7e64&units=metric`
     );
 
     const json = await  response.json()
@@ -19,28 +28,27 @@ const veriGetir = async ()=>{
     console.log(json);
     ekranaBastir(json)
 }
-veriGetir()
-//! ${data.weather[0].icon}
-function ekranaBastir(data) {
 
+
+function ekranaBastir(data) {
+console.log(data.weather[0].icon);
         document.querySelector("ul").innerHTML += `
-        <li class="list-unstyled">
+        <li class="list-unstyled ">
               <div class="card" style="width: 18rem" >
-                <img src="./img/sun-g999db51cc_640.png" class="card-img-top" alt="..." />
+                <img src="./img/${data.weather[0].icon}.png" class="card-img-top" alt="..." />
                 <div class="card-body">
-                  <h2>${data.name}<sup>${data.sys.country}</sup></h2>
+                  <h2>${data.name}<sup style="background-color: orange; padding:3px">${data.sys.country}</sup></h2>
                   <div>
                     ${data.main.temp} <sup>&#176;C</sup>
                   </div>
                   <figure>
                     <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
-                    <figcaption>${data.weather[0].main}</figcaption>
+                    <figcaption class="mb-3">${data.weather[0].description}</figcaption>
+                    <span> 🔼${data.main.temp_max}<sup>&#176;C</sup></span><span>🔽${data.main.temp_min}<sup>&#176;C</sup></span>
                   </figure>
                 </div>
               </div>
             </li>
-        
         `;
-
-
 }
+
