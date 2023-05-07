91c1a4ee08fe8258c758a8958cd17a6487458c8c
yunus <yunuspolat2528@gmail.com>
2023-05-07 17:56:08 +0200

    Weather App completed

--- Assignment_8_Weather_App/weather.js
+++ Assignment_8_Weather_App/weather.js
@@ -1,58 +1,129 @@
+const myInput = document.querySelector(".input1");
+const submitButton = document.querySelector(".submit");
+const warning = document.querySelector("p");
+const allCities = document.querySelector(".all-cities");
+const myLocation = document.querySelector(".location");
+const clearAllButton = document.querySelector(".clear-all");
 
-const cities = []
-const city = document.querySelector(".input1");
-const uyari = document.querySelector("p");
+//! language
+const langButton = document.querySelector(".languages");
 
-document.querySelector(".submit").onclick = () => {
-  if(city.value==""){
-    uyari.textContent = "Please enter a city"
-  }else if(cities.includes(city.value)){
-    uyari.textContent="You already have this city in your list"
-  }else{
-    uyari.textContent = ""
-    cities.unshift(city.value)
+let units = "metric";
+let lang = "en";
+let url;
+let cities = [];
 
-  veriGetir();
-  city.value = ""; // input kutusu bos gözükmesi icin.
-}
-};
+localStorage.setItem(
+  "apiKey",
+  EncryptStringAES("f56f703795a90532f7b92917734c7e64")
+);
+const apiKey = DecryptStringAES(localStorage.getItem("apiKey"));
+//! submit butonuna basinca olacaklar.
+submitButton.addEventListener("click", () => {
+  if (myInput.value) {
+    const cityName = myInput.value;
+    console.log(cityName);
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}&lang=${lang}`;
+    myInput.focus();
+    getWeatherDataFromApi();
+  }
+  myInput.value = "";
+  myInput.focus();
+});
 
-city.onkeydown = (taste) => {
+myInput.onkeydown = (taste) => {
   if (taste.keyCode === 13) {
-    document.querySelector(".submit").click();
+    submitButton.click();
   }
 };
 
-const veriGetir = async ()=>{
-    const response = await fetch(
-      //   `https://api.openweathermap.org/data/2.5/weather?q=london&appid=f56f703795a90532f7b92917734c7e64&units=metric`
-      `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=f56f703795a90532f7b92917734c7e64&units=metric`
-    );
+myLocation.addEventListener("click", () => {
+  navigator.geolocation?.getCurrentPosition(({ coords }) => {
+    const { latitude, longitude } = coords;
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}&lang=${lang}`;
+    getWeatherDataFromApi();
+  });
+});
 
-    const json = await  response.json()
-    ekranaBastir(json)
-}
+//todo ################  lang button
+langButton.addEventListener("click", (e) => {
+  if (e.target.id == "de") {
+    lang = "de";
+    myInput.setAttribute("placeholder", "🔍 Suche nach einer Stadt");
+    submitButton.innerHTML = "SUCHE";
+    clearAllButton.innerHTML = "Alles Löschen";
+  } else if (e.target.id == "en") {
+    lang = "en";
+    myInput.setAttribute("placeholder", "🔍 Search for a city");
+    submitButton.innerHTML = "SEARCH";
+    clearAllButton.innerHTML = "Clear all cities";
+  }
+  myInput.focus();
+});
 
+//todo clear all cities
+clearAllButton.addEventListener("click", () => {
+  allCities.innerHTML = "";
+  cities = [];
+  myInput.focus();
+});
 
-function ekranaBastir(data) {
-        document.querySelector("ul").innerHTML =
-          `
-        <li class="list-unstyled ">
-              <div class="card" style="width: 18rem" >
-                <img src="./img/${data.weather[0].icon}.png" class="card-img-top" alt="..." />
+const getWeatherDataFromApi = async () => {
+  try {
+    const response = await fetch(url).then((response) => response.json());
+    const { main, name, weather, sys } = response;
+    const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+
+    if (cities.indexOf(name) == -1) {
+      cities.unshift(name);
+      allCities.innerHTML =
+        `
+      <li class="city list-unstyled" id=${name}>
+        <button class="single-clear-btn btn btn-success">X</button>
+              <div class="card" style="width: 14rem" >
+                <img src="./img/${weather[0].icon}.png" class="card-img-top" alt="..." />
                 <div class="card-body">
-                  <h2>${data.name}<sup class="ms-2 fs-5 ps-1 pe-1" style="background-color: orange;">${data.sys.country}</sup></h2>
-                  <div>
-                    ${data.main.temp} <sup>&#176;C</sup>
+                  <h2>${name}<sup class="ms-2 fs-5 ps-1 pe-1" style="background-color: orange;">${sys.country}</sup></h2>
+                  <div class="city-temp fs-3 fw-bolder">
+                    ${Math.round(main.temp)} <sup>°C</sup>
                   </div>
                   <figure>
-                    <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
-                    <figcaption class="mb-3">${data.weather[0].description}</figcaption>
-                    <span> 🔼${data.main.temp_max}<sup>&#176;C</sup></span><span>🔽${data.main.temp_min}<sup>&#176;C</sup></span>
+                    <img class="city-icon" src="${iconUrlAWS}">
+                    <figcaption class="mb-3">${weather[0].description}</figcaption>
+                    <span> 🔼${main.temp_max}<sup>°C</sup></span><span>🔽${main.temp_min}<sup>°C</sup></span>
                   </figure>
                 </div>
               </div>
-            </li>
-        ` + document.querySelector("ul").innerHTML;
-}
+      </li>` + allCities.innerHTML;
+      //! single clear
+      const singleClearButton = document.querySelectorAll(".single-clear-btn");
+      singleClearButton.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+          delete cities[cities.indexOf(e.target.closest(".city").id)];
+          //todo cities.splice(cities.indexOf(e.target.closest(".city").id),1) //development aşamasında
+          e.target.closest(".city").remove();
+          console.log(cities);
+        });
+      });
+    } else {
+      if (lang == "de") {
+        warning.innerText = `Sie kennen das Wetter für die ${name} bereits. Bitte suchen Sie nach einer anderen Stadt 😉`;
+      } else {
+        warning.innerText = `You already know the weather for ${name}, Please search for another city 😉`;
+      }
 
+      setInterval(() => {
+        warning.innerText = "";
+      }, 3000);
+    }
+  } catch (error) {
+    if (lang == "de") {
+      warning.innerText = `Stadt nicht gefunden`;
+    } else {
+      warning.innerText = "City not found!";
+    }
+    setInterval(() => {
+      warning.innerText = "";
+    }, 3000);
+  }
+};
